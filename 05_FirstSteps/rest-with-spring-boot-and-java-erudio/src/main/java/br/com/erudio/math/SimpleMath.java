54ba0bf5eaa0e5aa7453c07e7e2b3a75aca538be
Victor Vasco <victordcvasco@gmail.com>
2024-04-18 22:25:31 -0300

    Section 6 End - Path Params and Exceptions

---
+++ 05_FirstSteps/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,24 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+
+	public Double sum(Double numberOne, Double numberTwo) {
+		return numberOne + numberTwo;
+	}
+
+	public Double div(Double numberOne, Double numberTwo) {
+		return numberOne / numberTwo;
+	}
+
+	public Double mult(Double numberOne, Double numberTwo) {
+		return numberOne * numberTwo;
+	}
+
+	public Double med(Double numberOne, Double numberTwo) {
+		return (numberOne + numberTwo) / 2;
+	}
+
+	public Double raiz(Double numberOne) {
+		return Math.sqrt(numberOne);
+	}
+}
